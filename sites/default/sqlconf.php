80ef05a23f319034d5fad1e1dd9e898b0892d2c5
Rod Roark <rod@sunsetsystems.com>
2015-07-31 11:23:33 -0700

    Made release for MTFC production.

--- sites/default/sqlconf.php
+++ sites/default/sqlconf.php
@@ -8,9 +8,9 @@ $disable_utf8_flag = false;
 
 $host  = 'localhost';
 $port  = '3306';
-$login = 'openemr2';
-$pass  = 'openemr2';
-$dbase = 'openemr2';
+$login = 'openemr3';
+$pass  = 'openemr3';
+$dbase = 'openemr3';
 
 $sqlconf = array();
 global $sqlconf;
